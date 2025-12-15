9c61706344be80f310bb4af0d211be70c6a2d645
Husam <husam05@users.noreply.github.com>
2025-12-15 06:50:01 +0300

    Hardcode basePath to Fix 404 error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/car-smart-park' : '',
-  assetPrefix: isProd ? '/car-smart-park/' : '',
+  basePath: '/car-smart-park',
   images: {
     unoptimized: true,
   },
